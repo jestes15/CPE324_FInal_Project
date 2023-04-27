439289884c9afdb420f69cf3abdad0e059ded62e
Joshua Estes <jne0010@uah.edu>
2023-04-27 17:29:15 -0500

    Update core.v

--- core.v
+++ core.v
@@ -1,25 +1,20 @@
-module core (
+module final (
 	input CLOCK_50,
 	input [7:0] time_in,
 	input set_hour, set_minute, set_second, set_mil, set_alarm, set_time,
 	output reg [6:0] hex7, hex6, hex5, hex4, hex3, hex2, hex1, hex0,
-	output alarm_sound
+	output reg alarm_sound
 );
-	reg [31:0] time_out, alarm;
-  	reg [19:0] size_of_file;
-  	reg signed [1:-14] wave_out;
-	reg signed [1:-14] two_dimension_array[0:19831];
-
-	initial begin
-		time_out = 0;
-		alarm = 0;
-		$readmemh("audio_02_16_signed.txt", two_dimension_array);
-	end
+	reg [31:0] time_out = 0;
+	reg [31:0] alarm = 0;
+	reg [31:0] intermediate = 0;
+	reg [31:0] alarm_timer = 0;
+	reg [31:0] alarm_counter = 0;
+	reg [31:0] base_count = 50_000_000 / 100;
+	reg [31:0] count = 50_000_000 / 100;
+	reg alarm_start = 0;
 
 	always @(posedge CLOCK_50 or posedge set_alarm or posedge set_time) begin
-		reg [25:0] base_count = 50000000 / 500000;
-		reg [25:0] count;
-		
 		if (set_time) begin
 			if (set_hour) begin
 				time_out[31:24] = time_in;
@@ -55,75 +50,66 @@ module core (
 			end
 		end
 		else begin
-			if (count) count = count + 1;
+		
+			if (count) count = count - 1;
+			
 			else begin
-				// Milliseconds
 				time_out[3:0] = time_out[3:0] + 1;
+				
+				// Milliseconds				
 				if (time_out[3:0] > 9) begin
 					time_out[3:0] = 0;
 					time_out[7:4] = time_out[7:4] + 1;
 				end
-
-				// Seconds
 				if (time_out[7:4] > 9) begin 
 					time_out[7:4] = 0;
 					time_out[11:8] = time_out[11:8] + 1;
 				end
-				if (time_out[11:8] > 5) begin 
+				
+				// Seconds
+				if (time_out[11:8] > 9) begin 
 					time_out[11:8] = 0;
 					time_out[15:12] = time_out[15:12] + 1;
 				end
-
-				// Minutes
-				if (time_out[15:12] > 9) begin 
+				if (time_out[15:12] > 5) begin 
 					time_out[15:12] = 0;
 					time_out[19:16] = time_out[19:16] + 1;
 				end
-				if (time_out[19:16] > 5) begin
+				
+				
+				// Minutes
+				if (time_out[19:16] > 9) begin
 						time_out[19:16] = 0;
 						time_out[23:20] = time_out[23:20] + 1;
 				end
-
+				if (time_out[23:20] > 5) begin
+						time_out[23:20] = 0;
+						time_out[27:24] = time_out[27:24] + 1;
+				end
+				
 				// Hours
-				if (time_out[27:23] > 9) begin
-						time_out[27:23] = 0;
+				if (time_out[27:24] > 9) begin
+						time_out[27:24] = 0;
 						time_out[31:28] = time_out[31:28] + 1;
-					end
-				if (time_out[31:28] == 2 &&  time_out[27:23] == 4) begin
-						time_out[31:23] = 0;
 				end
+				
+				if (time_out[31:28] == 2 &&  time_out[27:24] == 4) begin
+						time_out = 0;
+				end
+				
+				if (time_out == alarm) alarm_start = 1;
+				else alarm_start = 0;
+				
+				count = base_count;
 			end
 		end
 	end
 
 	always @(posedge CLOCK_50) begin
-		reg play_sound = 0;
-		reg count_down = 0;
-		reg [25:0] scoped_count = 0;
-		reg [25:0] base_count = 50000000 / 8000;
-		reg [6:0] index = 0;
-
-		if (alarm == time_out) begin 
-			play_sound = 1;
-			scoped_count = 19831;
-		end
-
-		if (play_sound) begin 
-			if (scoped_count) scoped_count = scoped_count - 1;
-			else begin
-				index = index + 1;
-				if (index > 19831) begin
-					play_sound = 0;
-					index = 0;
-				end
-				wave_out = two_dimension_array[index];
-				scoped_count = base_count;
-			end
-		end
-	end
-
-	always @(posedge CLOCK_50) begin
-		case (time_out[3:0])
+		if (set_alarm) intermediate = alarm;
+		else intermediate = time_out;
+	
+		case (intermediate[3:0])
 			0  : hex0 = 7'b1000000;
 			1  : hex0 = 7'b1111001;
 			2  : hex0 = 7'b0100100;
@@ -136,7 +122,7 @@ module core (
 			9  : hex0 = 7'b0011000;
 			default : hex0 = 7'bx; 
 		endcase
-			case (time_out[7:4])
+			case (intermediate[7:4])
 			0  : hex1 = 7'b1000000;
 			1  : hex1 = 7'b1111001;
 			2  : hex1 = 7'b0100100;
@@ -149,7 +135,7 @@ module core (
 			9  : hex1 = 7'b0011000;
 			default : hex1 = 7'bx; 
 		endcase
-		case (time_out[11:8])
+		case (intermediate[11:8])
 			0  : hex2 = 7'b1000000;
 			1  : hex2 = 7'b1111001;
 			2  : hex2 = 7'b0100100;
@@ -162,7 +148,7 @@ module core (
 			9  : hex2 = 7'b0011000;
 			default : hex2 = 7'bx; 
 		endcase
-		case (time_out[15:12])
+		case (intermediate[15:12])
 			0  : hex3 = 7'b1000000;
 			1  : hex3 = 7'b1111001;
 			2  : hex3 = 7'b0100100;
@@ -175,7 +161,7 @@ module core (
 			9  : hex3 = 7'b0011000;
 			default : hex3 = 7'bx; 
 		endcase
-		case (time_out[19:16])
+		case (intermediate[19:16])
 			0  : hex4 = 7'b1000000;
 			1  : hex4 = 7'b1111001;
 			2  : hex4 = 7'b0100100;
@@ -188,7 +174,7 @@ module core (
 			9  : hex4 = 7'b0011000;
 			default : hex4 = 7'bx; 
 		endcase
-		case (time_out[23:20])
+		case (intermediate[23:20])
 			0  : hex5 = 7'b1000000;
 			1  : hex5 = 7'b1111001;
 			2  : hex5 = 7'b0100100;
@@ -201,7 +187,7 @@ module core (
 			9  : hex5 = 7'b0011000;
 			default : hex5 = 7'bx; 
 		endcase
-		case (time_out[27:24])
+		case (intermediate[27:24])
 			0  : hex6 = 7'b1000000;
 			1  : hex6 = 7'b1111001;
 			2  : hex6 = 7'b0100100;
@@ -214,7 +200,7 @@ module core (
 			9  : hex6 = 7'b0011000;
 			default : hex6 = 7'bx; 
 		endcase
-		case (time_out[31:28])
+		case (intermediate[31:28])
 			0  : hex7 = 7'b1000000;
 			1  : hex7 = 7'b1111001;
 			2  : hex7 = 7'b0100100;
@@ -229,27 +215,73 @@ module core (
 		endcase
 	end
 
-	always @ (posedge CLOCK_50) begin
-		reg signed [3:-28] ge;
-		pdm(alarm_sound,wave_out,{2'b00,8'b1,6'd0},ge);
+	always @(posedge CLOCK_50) begin
+		reg [31:0] note = 0;
+		reg [15:0] frequency = 0;
+		
+		if (alarm_timer <= 1500) begin
+			get_data(note, frequency, 0);
+		end
+		
+		else if (alarm_timer <= 3000) begin
+			get_data(note, frequency, 1);
+		end
+		
+		else if (alarm_timer <= 4500) begin
+			get_data(note, frequency, 2);
+		end
+		
+		else if (alarm_timer <= 7000) begin
+			get_data(note, frequency, 4);
+		end
+		
+		
+		if (alarm_start || alarm_timer) begin
+			if (alarm_timer >= 7000 / 2)
+				alarm_timer = 0;
+					
+			if (alarm_counter <= note) begin
+				if (alarm_counter == note/2) begin
+					alarm_sound = 0;
+				end
+				alarm_counter = alarm_counter + 1;
+			end
+			else  begin
+				alarm_counter = 0;
+				alarm_sound = 1;
+				alarm_timer = alarm_timer + 1;
+			end
+		end
 	end
-
-	task pdm(
-		output d_out, 
-		input signed [1:-14] x, scale, 
-		inout signed [3:-28] ge
+	
+	task get_data(
+		output [31:0] note,
+		output [15:0] freq,
+		input index
 	);
-		`define ONE_32 {1'b0,3'b001,28'b0} 
-		reg signed [3:-28] x_total; 
-
-		x_total = x * scale;
-		if (x_total >= ge) begin
-			d_out = 1;
-			ge = ge + (`ONE_32 - x_total);
-		end
-		else begin
-			d_out = 0;
-			ge = ge - (`ONE_32 + x_total); 
-		end
+	
+	if (index == 0) begin
+		freq = 392;
+		note = 50000000 / 392;
+	end
+	else if (index == 1) begin
+		freq = 330;
+		note = 50000000 / 330;
+	end
+	else if (index == 2) begin
+		freq = 349;
+		note = 50000000 / 349;
+	end
+	else if (index == 3) begin
+		freq = 311;
+		note = 50000000 / 311;
+	end
+	else if (index == 4) begin
+		freq = 294;
+		note = 50000000 / 294;
+	end
+				
+	
 	endtask
+
 endmodule
